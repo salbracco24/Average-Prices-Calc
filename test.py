b34b2d1a5afe17f8e52620c3b67f5914316147bc
Salvatore Bracco <flyguyee@gmail.com>
2022-11-01 11:36:01 -0400

    Fixed gitignore

--- test.py
+++
@@ -1,6 +0,0 @@
-prices = [('MATIC-USD', 10.52, 1057.53), ('ALGO-USD', 2000.23, 500.117), ('SHIT-USD', "ERROR", "ERROR"), ('BTC-USD', "--------", "--------")]
-authInfo = ['Ron Bracco', '76565756', 'fsdssfd', 'gfdsdg','Sal']
-lineSep = '----------------------------------------------------------'
-
-print(lineSep)
-print(authInfo[0:3])
